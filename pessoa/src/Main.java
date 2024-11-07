28e9175470bb50ea7b2e1b784849730b0bbf09d4
Gabriel Arantes <gabrielpacontato@gmail.com>
2024-11-07 19:59:18 -0300

    Exercício 1

--- pessoa/src/Main.java
+++ pessoa/src/Main.java
@@ -1,6 +1,5 @@
 import domain.Pessoa;
 import exception.DataFuturaException;
-import service.PessoaService;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -8,31 +7,29 @@ import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) throws DataFuturaException {
 
         try {
-            PessoaService pessoaService = new PessoaService();
             Pessoa pessoa = new Pessoa();
             Scanner sc = new Scanner(System.in);
 
-            System.out.print("Informe seu nome:\n");
+            System.out.print("Informe seu nome: ");
             pessoa.setNome(sc.nextLine());
 
-            System.out.print("Informe sua data de nascimento:\n");
             System.out.print("Utilize o formato dd/MM/yyy HH:mm\n");
+            System.out.print("Informe sua data de nascimento: ");
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyy HH:mm");
             LocalDateTime dateTime = LocalDateTime.parse(sc.nextLine(), formatter);
             pessoa.setDataNascimento(dateTime);
 
-            Integer idade = pessoaService.calcularIdade(pessoa);
+            int idade = LocalDateTime.now().getYear() - pessoa.getDataNascimento().getYear();
 
-            if (idade < 0) {
+            if (idade == 0) {
                 throw new DataFuturaException("Informe uma data anterior a de hoje.");
             }
 
             if (idade < 1) {
-                pessoa.getDataNascimento().until(LocalDateTime.now(), ChronoUnit.MONTHS);
+                System.out.print("A idade é: " + pessoa.getDataNascimento().until(LocalDateTime.now(), ChronoUnit.MONTHS) + " meses");
             } else System.out.print(pessoa.getNome() + ", você tem " + idade + " anos.");
 
         } catch (DataFuturaException e) {
@@ -40,5 +37,4 @@ public class Main {
         }
 
     }
-
 }
